0cd73f47f3196ce56bcda13debda83db04ac0c07
Raymond Lam <raymond.m.lam@gmail.com>
2022-05-15 12:17:39 +0200

    console logging socket

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import socketio from 'socket.io-client'
 const socket = socketio.connect(process.env.REACT_APP_SOCKET_URL)
 
 function App() {
-	console.log(socket.id)
+	console.log(socket)
 
 	return (
 		<div className='App'>
