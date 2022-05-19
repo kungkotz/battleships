8c8d2fcaa43777dd1e672a31a423c809a262554b
GustafGronlund <gustafgronlund@gmail.com>
2022-05-19 17:13:41 +0200

    Changed structure on gameboard.

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,95 +1,175 @@
 import "../styles/GameBoard.scss";
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
 const GameBoard = ({ socket, username, opponentName }) => {
-	const [leftGame, setLeftGame] = useState(false);
-	const [guess, setGuess] = useState(0)
-	const [gameStatus, setGameStatus] = useState(true)
-	const yourDivBoxes = []
-	const enemyDivBoxes = [];
-	const yourShips = ['y0', 'y1', 'y2', 'y3']
-	const enemyShips = ['e1', 'e2', 'e3', 'e4'];
+  const [leftGame, setLeftGame] = useState(false);
+  const [enemyDivs, setEnemyDivs] = useState([]);
+  const [yourDivs, setYourDivs] = useState([]);
+  //   const [guess, setGuess] = useState(0);
+  //   const [gameStatus, setGameStatus] = useState(true);
+  const yourShips = [];
+  const enemyShips = [];
 
-	// if (gameStatus === true) {
-	// 	console.log('här försvinner div och spelet börjar')
-	// }
+  // if (gameStatus === true) {
+  // 	console.log('här försvinner div och spelet börjar')
+  // }
 
-	// if (gameStatus === false) {
-	// 	console.log("div som täcker tills motståndare är ansluten")
-	// }
+  // if (gameStatus === false) {
+  // 	console.log("div som täcker tills motståndare är ansluten")
+  // }
 
-	const postBoxClick = (id, clicked) => {
-		if (clicked) {
-			document.querySelector(`.${id}`).style.backgroundColor = "green"
-			document.querySelector(`.${id}`).style.pointerEvents = "none"
-		}
+  const generateShips = (squares, player) => {
+    let ship = [];
 
-		if (!clicked) {
-			document.querySelector(`.${id}`).style.backgroundColor = "red"
-			document.querySelector(`.${id}`).style.pointerEvents = "none"
-		}
-	}
+    let randomPosition = Math.floor(Math.random() * 100);
 
-	const clickOnGrid = (e) => {
-		setGuess(guess + 1)
+    if (player === "you") {
+      let startPosition = "y" + randomPosition;
 
-		if (enemyShips.includes(e.target.className)) {
-			postBoxClick(e.target.className, true)
-		}
+      ship.push(startPosition);
 
-		if (!enemyShips.includes(e.target.className)) {
-			postBoxClick(e.target.className, false)
-		}
-	}
+      for (let i = 0; i < squares - 1; i++) {
+        //   console.log("e" + randomPosition++);
+        ship.push("y" + ++randomPosition);
+      }
 
-	for (let i = 0; i < 100; i++) {
-		yourDivBoxes.push(<div className={`y${i}`} key={`${i}`}></div >)
-	}
+      if (yourShips[ship]) {
+        generateShips();
+      }
 
-	for (let i = 0; i < 100; i++) {
-		enemyDivBoxes.push(<div className={`e${i}`} onClick={clickOnGrid} key={`${i}`}></div >)
-	}
+      console.log(ship);
 
-	// yourShips.forEach(e => {
-	// 	console.log(e)
-	// 	document.querySelector(`.${e}`).style.backgroundColor = 'blue'
-	// })
+      return [...ship];
+    }
 
-	useEffect(() => {
-		socket.on('player:disconnected', function (boolean) {
-			setLeftGame(boolean);
-		});
-	}, [socket]);
+    if (player === "enemy") {
+      let startPosition = "e" + randomPosition;
 
-	return (
-		<div>
-			<div>
-				<h2>Let's play some Battleship!</h2>
+      ship.push(startPosition);
 
+      for (let i = 0; i < squares - 1; i++) {
+        //   console.log("e" + randomPosition++);
+        ship.push("e" + ++randomPosition);
+      }
 
-				<p>{username} here</p>
-				<p> {opponentName} somewhere</p>
+      if (enemyShips.includes(ship)) {
+        generateShips();
+      }
 
-				{/* Lets player know if opponent left game */}
-				{leftGame === true && <h1>{opponentName} left the game</h1>}
-			</div>
-			<main>
-				<section>
-					<p>Your guesses: {guess}</p>
-					<div className="yourBoard">
-						{yourDivBoxes}
-					</div>
-				</section>
-				<section>
-					<p>Enemy :P click on this board</p>
-					<div className="enemyBoard">
-						{enemyDivBoxes}
-					</div>
-				</section>
-			</main>
+      console.log(ship);
 
-		</div>
-	)
-}
+      return ship;
+    }
+  };
 
-export default GameBoard
+  console.log("dina skepp", yourShips);
+
+  const postBoxClick = (id, clicked) => {
+    if (clicked) {
+      document.querySelector(`.${id}`).style.backgroundColor = "green";
+      document.querySelector(`.${id}`).style.pointerEvents = "none";
+    }
+
+    if (!clicked) {
+      document.querySelector(`.${id}`).style.backgroundColor = "red";
+      document.querySelector(`.${id}`).style.pointerEvents = "none";
+    }
+  };
+
+  const clickOnGrid = (e) => {
+    // setGuess(guess + 1);
+
+    if (enemyShips.includes(e.target.className)) {
+      postBoxClick(e.target.className, true);
+    }
+
+    if (!enemyShips.includes(e.target.className)) {
+      postBoxClick(e.target.className, false);
+    }
+  };
+
+  const generateYourDivs = () => {
+    const yourDivBoxes = [];
+    for (let i = 0; i < 100; i++) {
+      if (yourShips.includes("y" + i)) {
+        yourDivBoxes.push(
+          <div className={`y${i}`} key={`${i}`}>
+            {i + " ⛵️"}
+          </div>
+        );
+      } else {
+        yourDivBoxes.push(
+          <div className={`y${i}`} key={`${i}`}>
+            {i}
+          </div>
+        );
+      }
+    }
+    setYourDivs((yourDivs) => [...yourDivs, ...yourDivBoxes]);
+  };
+
+  const generateEnemyDivs = () => {
+    const enemyDivBoxes = [];
+    for (let i = 0; i < 100; i++) {
+      enemyDivBoxes.push(
+        <div className={`e${i}`} onClick={clickOnGrid} key={`${i}`}>
+          {i}
+        </div>
+      );
+    }
+    setEnemyDivs((enemyDivs) => [...enemyDivs, ...enemyDivBoxes]);
+  };
+
+  console.log("dina skepp igen ", enemyDivs);
+
+  //   document.querySelector(".y0").innerHTML = "hej";
+
+  //   yourShips.forEach((e) => {
+  //     console.log(e);
+  //     // document.querySelector(`.${e}`).innerHTML = "⛵️";
+  //     document.querySelector(".y1").innerHTML = "hej";
+  //   });
+
+  useEffect(() => {
+    console.log("körs det ens");
+    generateYourDivs();
+    // console.log("i effect", enemyDivBoxes);
+    generateEnemyDivs();
+    // yourShips.push(...generateShips(4, "you"));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  //   console.log("utanför effect", enemyDivBoxes);
+
+  //   useEffect(() => {
+  //     socket.on("player:disconnected", function (boolean) {
+  //       setLeftGame(boolean);
+  //     });
+  //   }, [socket]);
+
+  return (
+    <div>
+      <div>
+        <h2>Let's play some Battleship!</h2>
+
+        <p>{username} here</p>
+        <p> {opponentName} somewhere</p>
+
+        {/* Lets player know if opponent left game */}
+        {leftGame === true && <h1>{opponentName} left the game</h1>}
+      </div>
+      <main>
+        <section>
+          <p>Your guesses: </p>
+          <div className="yourBoard">{yourDivs}</div>
+        </section>
+        <section>
+          <p>Enemy :P click on this board</p>
+          <div className="enemyBoard">{enemyDivs}</div>
+        </section>
+      </main>
+    </div>
+  );
+};
+
+export default GameBoard;
